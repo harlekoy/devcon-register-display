95302554232039f8404c5eb8061621e9bed5efd9
Harlequin Doyon <harlequin.doyon@gmail.com>
2018-11-21 15:55:05 +0800

    Added firebase functionality

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li v-for="attendee in attendees">{{ attendee.profile.first_name }} {{ attendee.profile.last_name }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mapState, mapActions } from 'vuex'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  created () {
+    this.init()
+  },
+
+  computed: {
+    ...mapState(['attendees']),
+  },
+
+  methods: {
+    ...mapActions(['init'])
   }
 }
 </script>
